2eb6891b1b3c1198ddd8cb97c66b391f34426687
adelliyaarmayni <adelliyaarmayni25@gmail.com>
2025-07-13 20:45:12 +0700

    update: input

--- resources/views/guru/hafalansantri/input.blade.php
+++ resources/views/guru/hafalansantri/input.blade.php
@@ -101,7 +101,6 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
-          <!-- input hafalan -->
           <h1 class="text-lg md:text-xl font-bold">Input Hafalan Santri</h1>
         </div>
         <img src="{{ asset('img/image/logortq.png') }}" alt="Logo" class="h-14 md:h-20 bg-white p-2 rounded" />
